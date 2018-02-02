236da586921ed74b6b63955fd9e4835eeef5d14f
LordMouton <32259619+LordMouton@users.noreply.github.com>
2018-02-02 12:16:56 +0100

    Update barometre.R
    ajout d'une fonction de recherches de mots et ajout de compteurs d'occurences des mots et rues à la fin

--- barometre.R
+++ barometre.R
@@ -2,7 +2,7 @@
 # ouvertes du baromètre des villes cyclables de la FUB, édition 2017
 #
 library(stringr)
-
+library(dplyr)
 # Lire les données
 # l'argument 'ville' doit correspondre au nom du fichier à lire, sans le .csv
 lire_donnees <- function (ville){
@@ -13,17 +13,24 @@ lire_donnees <- function (ville){
 # Fonction qui cherche des noms de rue dans un texte
 trouve_rues <- function(texte){
   pattern_rue <- "([Rr]ue|[Aa]venue|[Bb]oulevard|[Pp]lace|[Rr]oute) (de|du|des)? (la|le)? [:alpha:]+"
-  str_extract_all(texte[,2],pattern_rue)
+  rues <- str_extract_all(texte[,2],pattern_rue)
+  as.tibble(str_to_lower(rues)) #met tout en minuscule pour homogénéiser
 }
 
 # Fonction qui cherche juste des mots de 4 lettres ou plus
 trouve_mots <- function(texte){
-  pattern_mot <- "
+  pattern_mot <- "[:alpha]{4,}"
+  mots <- str_extract_all(texte[,3],pattern_mot)
+  as.tibble(str_to_lower(mots)) #met tout en minuscule pour homogénéiser
 }
 
-# Script
+# Script - lecture des données, recherche des patterns
 data_paris <- lire_donnees("Paris")
 rues_paris <- trouve_rues(data_paris)
+mots_paris <- trouve_mots(data_paris)
+# analyse des données extraites
+count_rues_paris <- count(rues_paris)
+count_mots_paris <- count(mots_paris)
 
 #reste à faire: enlever les données vides ; vérifier la syntaxe du pattern de recherche; écrire les données
 # dans un fichier lisible ; trouver les points noirs avec le plus grand nombre d'occurences
