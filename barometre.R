d3d4b97e2755d4cd108bda5bb18d0354125da8d2
LordMouton <32259619+LordMouton@users.noreply.github.com>
2018-02-02 11:56:53 +0100

    Update barometre.R

--- barometre.R
+++ barometre.R
@@ -1,10 +1,10 @@
-# Ce script tente d'extraire des données pertinents des réponses aux deux questions
-# ouvertes du baromètre des villes cyclables de la FUB, édition 2017
+# Ce script tente d'extraire des donnÃ©es pertinents des rÃ©ponses aux deux questions
+# ouvertes du baromÃ¨tre des villes cyclables de la FUB, Ã©dition 2017
 #
 library(stringr)
 
-# Lire les données
-# l'argument 'ville' doit correspondre au nom du fichier à lire, sans le .csv
+# Lire les donnÃ©es
+# l'argument 'ville' doit correspondre au nom du fichier Ã  lire, sans le .csv
 lire_donnees <- function (ville){
   nomfic <- paste(ville,".csv", sep="")
   data <- read.csv2(nomfic, stringsAsFactors = F)
@@ -12,14 +12,19 @@ lire_donnees <- function (ville){
 #
 # Fonction qui cherche des noms de rue dans un texte
 trouve_rues <- function(texte){
-  pattern_rue <- "([Rr]ue|[Aa]venue|[Bb]oulevard|[Pp]lace|[Rr]oute) (de|du|des)? (la|le)? [a-zA-Zéèçàù]+"
+  pattern_rue <- "([Rr]ue|[Aa]venue|[Bb]oulevard|[Pp]lace|[Rr]oute) (de|du|des)? (la|le)? [:alpha:]+"
   str_extract_all(texte[,2],pattern_rue)
+}
+
+# Fonction qui cherche juste des mots de 4 lettres ou plus
+trouve_mots <- function(texte){
+  pattern_mot <- "
 }
 
 # Script
 data_paris <- lire_donnees("Paris")
 rues_paris <- trouve_rues(data_paris)
 
-#reste à faire: enlever les données vides ; vérifier la syntaxe du pattern de recherche; écrire les données
+#reste Ã  faire: enlever les donnÃ©es vides ; vÃ©rifier la syntaxe du pattern de recherche; Ã©crire les donnÃ©es
 # dans un fichier lisible ; trouver les points noirs avec le plus grand nombre d'occurences
 #write.csv2(rues_paris,file="rues.csv")
